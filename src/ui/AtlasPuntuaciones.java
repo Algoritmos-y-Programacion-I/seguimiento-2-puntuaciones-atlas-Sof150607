18c2380015fdbaa9c4a0bc8b5ec82dc9760a22af
Sofia <sofiaorejuelavelez06@gmail.com>
2025-03-07 12:39:27 -0500

    Avance

--- src/ui/AtlasPuntuaciones.java
+++ src/ui/AtlasPuntuaciones.java
@@ -1,13 +1,26 @@
 // Análisis (completar)
-// Descripción del programa:
-// Entradas:
-// Salidas:
-// Ejemplo:
+// Descripción del programa: El programa tiene como finalidad gestionar las puntuaciones de los jugadores en un torneo. Para cada 
+// jugador, se registran sus puntajes en cada ronda y al final se deben calcular y mostrar estadísticas.
+
+// Entradas: Cantidad de jugadores(máximo 10), cantidad de rondas (máximo 5), Puntaje de cada uno en cada ronda (num entre 0-100)
+// Salidas:Puntaje total del jugador, Promedio total del jugador, Jugador con puntuación más alta 
+
+// Ejemplo: Se indica que jugarán 2 jugadores en 3 rondas, 
+//jugador 1 ronda1: 30
+//jugador 1 ronda2: 50
+//jugador 1 ronda3: 40
+//jugador 2 ronda1: 20
+//jugador 2 ronda2: 20
+//jugador 2 ronda3: 30
+//Jugador 1 - Puntaje Total: 120, Promedio: 40
+// Jugador 2 - Puntaje Total: 70, Promedio: 23.3
+
+// El jugador con la puntuación más alta es: Jugador 1
 
 
-package ui; // No olvidar especificar el paquete / carpeta de nuestro programa
+package ui; 
 
-import java.util.Scanner; // Para poder usar Scanner, debemos importarlo
+import java.util.Scanner; 
 
 public class AtlasPuntuaciones {
 
@@ -15,6 +28,10 @@ public class AtlasPuntuaciones {
 	private Scanner escaner;
 
 	// Declaración de nuestras constantes a utilizar
+
+	 final int CANT_MAX_JUGADORES = 10;
+	final int CANT_MAX_RONDAS = 5;
+
 
 
 	private AtlasPuntuaciones() {
@@ -27,9 +44,42 @@ public class AtlasPuntuaciones {
 		// Declaracion de todas las variables a usar en el programa
 
 
+
+		
+
+
+
+
 		// Notificacion al usuario de la solicitud de dato
 
-		// Capturamos el dato con nuestro Scanner (que se llama, escaner)
+		 System.out.println("Bienvenido al torneo de videouegos");
+
+		 System.out.println("Por favor, ingrese la cantidad de jugadores (Máx. 10):");
+         int cantJugadores = escaner.nextInt();
+
+		System.out.println("Ahora, ingrese la cantidad de rondas a jugar (Máx. 5):");
+         int cantRondas = escaner.nextInt();
+
+
+		int[] Jugadores = new int[cantJugadores];
+		int[] Puntuaciones = new int[cantRondas][cantJugadores];
+
+
+
+		for(int i = 0; i < cantJugadores; i++) {
+
+			for (int j = 0; j < cantRondas; j++) {
+            	System.out.println("Jugador " + (i+1) + ", escriba su puntaje de la ronda:" + (j+1) );
+            	Puntuaciones[i][j] = escaner.nextInt();
+			}
+        }
+
+		// for ()
+
+		
+
+		}
+
 
 
 
@@ -51,8 +101,10 @@ public class AtlasPuntuaciones {
 	 * @param int[] numeros
 	 * @return 
 	 */
-    public int calcularSumaTotal(int[] numeros) {
-        // Completar
+    public int calcularSumaTotal(int[] Puntuaciones) {
+       int suma = 0;
+
+	   return 0;
     }
 
 	/**
@@ -61,7 +113,7 @@ public class AtlasPuntuaciones {
 	 * @return 
 	 */
     public double calcularPromedio(int[] numeros) {
-        // Completar
+        return 0;
     }
 
 	/**
@@ -70,10 +122,9 @@ public class AtlasPuntuaciones {
 	 * @return 
 	 */
     public double encontrarMayor(double[] numeros) {
-        // Completar
+        return 0;
     }
 
 	
 
 	
-}
